6b5e3562862dfb00a5041019f37ef8e963f8c101
jorge <jorge.perez01epn@gmail.com>
2024-09-27 12:58:23 -0500

    Inicial setup structure of Portfolio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+
+{
+  /* <div className='absolute top-28 left-0 right-0 z-10 flex justify-center items-center'>
+  POPUP
+</div> */
+}
+
+const Home = () => {
+  return (
+    <section className='w-full h-screen relative'>
+      <Canvas></Canvas>
+    </section>
+  );
+};
+
+export default Home;
